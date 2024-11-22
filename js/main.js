411319a43001a5d3b6d236090c8c6c032594f5b0
Alinur Shayakhmet <vashalinur@gmail.com>
2024-11-23 03:58:11 +0500

    changed some layouts with styles

--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-	gsap.registerPlugin(ScrollTrigger);
-
-	const scroll = new LocomotiveScroll({
-		el: document.querySelector("[data-scroll-container]"),
-		smooth: true,
-	});
-
-	scroll.on("scroll", ScrollTrigger.update);
-
-	ScrollTrigger.scrollerProxy("[data-scroll-container]", {
-		scrollTop(value) {
-			return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-		},
-		getBoundingClientRect() {
-			return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-		},
-		pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
-	});
-
-	ScrollTrigger.addEventListener("refresh", () => scroll.update());
-	ScrollTrigger.refresh();
+window.onload = function () {
 
 	function applyAnimations() {
 		const screenWidth = window.innerWidth;
-		console.log("Applying animations for screen width:", screenWidth);
 
 		if (screenWidth >= 1200) {
 			gsap.from(".menu__list li", {
@@ -33,56 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
 				stagger: 0.2,
 			});
 			gsap.from(".first-main-section__left h2", { duration: 1, opacity: 0.2, x: -30 });
-			gsap.from(".about__me-item", { duration: 1, opacity: 0, y: 30, stagger: 0.25 });
-			gsap.from(".second-main-section__right", { duration: 1, opacity: 0.2, x: 30 });
 		} else if (screenWidth >= 992) {
-			gsap.from(".first-main-section__left", { duration: 1, opacity: 0, x: -20 });
-			gsap.from(".about__me-item", { duration: 1, opacity: 0, y: 20, stagger: 0.25 });
-			gsap.from(".first-main-section__right", { duration: 1, opacity: 1, x: 20 });
+			gsap.from(".first-main-section__left h2", { duration: 1, opacity: 0, x: -20 });
 		} else if (screenWidth >= 768) {
-			gsap.from(".first-main-section__left", { duration: 1, opacity: 0, x: -15 });
-			gsap.from(".about__me-item", { duration: 1, opacity: 0, y: 15, stagger: 0.25 });
-			gsap.from(".first-main-section__right", { duration: 1, opacity: 1, x: 15 });
+			gsap.from(".first-main-section__left h2", { duration: 1, opacity: 0, x: -15 });
 		} else {
-			gsap.from(".first-main-section__left", { duration: 1, opacity: 0, y: -15 });
-			gsap.from(".about__me-item", { duration: 1, opacity: 0, y: 15, stagger: 0.25 });
-			gsap.from(".first-main-section__right", { duration: 1, opacity: 1, x: 15 });
+			gsap.from(".first-main-section__left h2", { duration: 1, opacity: 0, y: -15 });
 		}
 	}
 
-	gsap.to(".first-main-section__right img, .second-main-section__left", {
+	applyAnimations();
+
+	let resizeTimeout;
+	window.addEventListener("resize", () => {
+		clearTimeout(resizeTimeout);
+		resizeTimeout = setTimeout(applyAnimations, 200);
+	});
+
+	gsap.to(".first-main-section__right img", {
 		y: -10,
 		repeat: -1,
 		yoyo: true,
 		ease: "power1.inOut",
 		duration: 2,
 	});
-
-	// gsap.to(".reviews__section h3", {
-	// 	duration: 2,
-	// 	text: "отзывы",
-	// 	ease: "none",
-	// });
-
-	// gsap.fromTo(".services-section-cards__item",
-	// 	{ opacity: 0, y: 50 },
-	// 	{
-	// 		opacity: 1,
-	// 		y: 0,
-	// 		scrollTrigger: {
-	// 			trigger: ".services-section-cards__item",
-	// 			start: "top 80%",
-	// 			end: "bottom 20%",
-	// 			scrub: true,
-	// 			toggleActions: "play reverse play reverse",
-	// 		}
-	// 	}
-	// );
-
-	applyAnimations();
-
-	window.addEventListener("resize", applyAnimations);
-});
+};
 
 var swiper = new Swiper(".swiper", {
 	slidesPerView: 3,
